39a3b7de765c7b9f56d339241992bd6e9c3b6fc0
bbrfkr <bbrfkr@gmail.com>
2019-01-09 11:47:24 +0900

    bug fix

--- scripts/ansibleconfigserver_bootstrap.sh
+++ scripts/ansibleconfigserver_bootstrap.sh
@@ -49,7 +49,7 @@ qs_retry_command 10 yum -y install jq
 
 qs_retry_command 10 yum -y install yum-versionlock
 
-qs_retry_command 10 yum -y install ansible-${ANSIBLE_VERSION}
+qs_retry_command 10 yum -y install ansible
 
 yum versionlock add ansible
 sed -i 's/#host_key_checking = False/host_key_checking = False/g' /etc/ansible/ansible.cfg
